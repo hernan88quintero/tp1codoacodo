84a47ca0cdb92a10cf8a8817acb5fca3fed310cd
Hernan Quintero <quinteroqhernane@gmail.com>
2022-05-17 21:22:23 -0300

    manipulando el DOM con JS -formulario-

--- js/compra.js
+++ js/compra.js
@@ -1,24 +1,50 @@
+// llamando a la función a través del evento "click"
 
-var cantidad = 500;
+document.getElementById("resumenBtn").addEventListener("click", captar);
 
+function captar(){
+      let nombre = document.getElementById('nombre').value;
+      let apellido = document.getElementById('apellido').value;
+      let correo = document.getElementById('correo').value;
+      let cantidad = parseInt(document.getElementById('cantidad').value);
+      let categoria = parseInt(document.getElementById('categoria').value);
+      let precio = 200;
 
-function tickets(cantidad){
-    if (cantidad >= 1) {
-        let total = cantidad * 200;
-        console.log(total);
-    }
+      //Función que capta la cantidad de tickets a comprar
+
+      function total(cantidad){
+            if (cantidad >= 1) {
+                let total = cantidad * precio;
+                return(total);
+            }else{
+                  alert("Por favor Ingrese una cantidad válida de tickets.")
+            }
+        };
+
+      //Función que capta y procesa el descuento seleccionado
+
+      function descuento(categoria){
+            if(categoria === 1){
+                  let estudiante = total(cantidad) * 0.80;
+                  return total(cantidad) - estudiante;
+            }else if(categoria === 2){
+                  let trainee = total(cantidad) * 0.50;
+                  return total(cantidad) - trainee;
+            }else if(categoria === 3){
+                  let junior = total(cantidad) * 0.15;
+                  return total(cantidad) - junior;
+            }else{
+                  alert("Por favor seleccione una categoría");
+            }
+      };
+
+      // llamado de la función e impresión del resultado en pantalla
+      
+      var mostrarEnPantalla = document.getElementById('mostrar');
+      mostrarEnPantalla.innerHTML += descuento(categoria);
+
+      // llamado de los valores e impresión del resultado en consola
+
+      // console.log("Nombre: " + nombre + " Apellido: " + apellido + " Correo: " + correo + " Cantidad: " + cantidad + " Categoria: " + categoria + " total: " + total(cantidad) + " total con descuento: " + descuento(categoria));
 
 }
-
-tickets(cantidad);
-
-
-
-function seleccionarCategoria(categoria, callback) {
-    if (categoria === 'Estudiante') {
-        console.log ("descuento 80 %");
-        tickets(cantidad) * 0.8;
-    }
-}
-
-seleccionarCategoria('Estudiante');
